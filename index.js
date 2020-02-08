22cabf435bc262523050b0c1c231efc81d56abda
merlox <merunasgrincalaitis@gmail.com>
2020-02-08 10:13:34 +0100

    Completed app

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const { app, BrowserWindow } = require('electron')
-const fetch = require('node-fetch')
-const clientId = '2f8d10fd37bf'
-const clientSecret = '044668c4933995815fea35d03d2c40daebf58965'
 let myWindow
 
 app.on('ready', () => {
   myWindow = new BrowserWindow({
     width: 1000,
-    height: 700,
+    height: 850,
     webPreferences: {
       nodeIntegration: true,
     },
